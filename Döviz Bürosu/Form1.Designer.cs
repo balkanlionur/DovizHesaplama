1f5680a0c097e930f19423f0d8fd8da945136c4a
ONUR BALKANLI <onur.balkanli@hotmail.com>
2017-08-02 12:26:13 +0300

    Güncel Döviz Hesaplama

---
+++ Döviz Bürosu/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Döviz_Bürosu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtsonuc = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(78, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(208, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(59, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tutar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(38, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Para Birimi:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(26, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Karşı Para Birimi:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "TÜRK LİRASI",
+            "ABD DOLARI",
+            "EURO",
+            "AVUSTRALYA DOLARI",
+            "DANİMARKA KRONU",
+            "İNGİLİZ STERLİNİ",
+            "İSVİÇRE FRANGI",
+            "İSVEÇ KRONU",
+            "KANADA DOLARI",
+            "KUVEYT DİNARI",
+            "NORVEÇ KRONU",
+            "S. ARABİSTAN RİYALİ",
+            "BULGAR LEVASI",
+            "RUMEN LEYİ",
+            "RUS RUBLESİ",
+            "ÇİN YUANI"});
+            this.comboBox1.Location = new System.Drawing.Point(172, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(134, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "TÜRK LİRASI",
+            "ABD DOLARI",
+            "EURO",
+            "AVUSTRALYA DOLARI",
+            "DANİMARKA KRONU",
+            "İNGİLİZ STERLİNİ",
+            "İSVİÇRE FRANGI",
+            "İSVEÇ KRONU",
+            "KANADA DOLARI",
+            "KUVEYT DİNARI",
+            "NORVEÇ KRONU",
+            "S. ARABİSTAN RİYALİ",
+            "BULGAR LEVASI",
+            "RUMEN LEYİ",
+            "RUS RUBLESİ",
+            "ÇİN YUANI"});
+            this.comboBox2.Location = new System.Drawing.Point(172, 118);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(134, 21);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(19, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hesaplanan Sonuc:";
+            // 
+            // txtsonuc
+            // 
+            this.txtsonuc.Location = new System.Drawing.Point(172, 145);
+            this.txtsonuc.Name = "txtsonuc";
+            this.txtsonuc.Size = new System.Drawing.Size(134, 20);
+            this.txtsonuc.TabIndex = 17;
+            this.txtsonuc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(366, 284);
+            this.Controls.Add(this.txtsonuc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Döviz Hesaplama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtsonuc;
+    }
+}
+
---
+++ Döviz Bürosu/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Döviz_Bürosu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float para1, para2, tutar;
+ 
+        string para,parabirimi1,parabirimi2,arapara;
+
+        public void parabirimi(string veri )
+        {
+            string sonuc = " ";
+            switch (veri)
+            {
+                case "TÜRK LİRASI": sonuc = "TL"; break;
+                case "ABD DOLARI": sonuc = "USD"; break;
+                case "EURO": sonuc = "EUR"; break;
+                case "AVUSTRALYA DOLARI": sonuc = "AUD"; break;
+                case "DANİMARKA KRONU": sonuc = "DKK"; break;
+                case "İNGİLİZ STERLİNİ": sonuc = "GBP"; break;
+                case "İSVİÇRE FRANGI": sonuc = "CHF"; break;
+                case "İSVEÇ KRONU": sonuc = "SEK"; break;
+                case "KANADA DOLARI": sonuc = "CAD"; break;
+                case "KUVEYT DİNARI": sonuc = "KWD"; break;
+                case "NORVEÇ KRONU": sonuc = "NOK"; break;
+                case "S.ARABİSTAN RİYALİ": sonuc = "SAR"; break;
+                case "BULGAR LEVASI": sonuc = "BGN"; break;
+                case "RUMEN LEYİ": sonuc = "RON"; break;
+                case "RUS RUBLESİ": sonuc = "RUB"; break;
+                case "ÇİN YUANI": sonuc = "CNY"; break;
+            }
+            
+            para = sonuc;
+            
+        }
+      
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string bugun = "http://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldoc = new XmlDocument();
+            xmldoc.Load(bugun);
+            if ((comboBox1.Text == "TÜRK LİRASI") && (comboBox2.Text == "TÜRK LİRASI"))
+            {
+                tutar = float.Parse(textBox1.Text);
+                txtsonuc.Text = tutar.ToString();
+            }
+            else if (comboBox2.Text == "TÜRK LİRASI")
+            {
+                parabirimi(comboBox1.Text);
+                parabirimi1 = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='" + para + "']/ForexBuying").InnerXml;
+                parabirimi1 = parabirimi1.Replace(".", ",");
+                parabirimi1 = parabirimi1.Substring(0, parabirimi1.Length - 1);
+                tutar = float.Parse(textBox1.Text);
+                para1 = float.Parse(parabirimi1) * tutar;
+                txtsonuc.Text = para1.ToString();
+            }
+            else if(comboBox1.Text == "TÜRK LİRASI")
+            {
+                parabirimi(comboBox2.Text);
+                string parabirimi2 = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='" + para + "']/ForexBuying").InnerXml;
+                parabirimi2 = parabirimi2.Replace(".", ",");
+
+                parabirimi2 = parabirimi2.Substring(0, parabirimi2.Length - 1);
+                tutar = float.Parse(textBox1.Text);
+                para2 = tutar / float.Parse(parabirimi2);
+
+                txtsonuc.Text = para2.ToString();
+            }
+            
+            else
+            {
+                //tl cevirme
+                parabirimi(comboBox1.Text);
+                parabirimi1 = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='" + para + "']/ForexBuying").InnerXml;
+                parabirimi1 = parabirimi1.Replace(".", ",");
+                parabirimi1 = parabirimi1.Substring(0, parabirimi1.Length - 1);
+
+                tutar = float.Parse(textBox1.Text);
+                para1 = float.Parse(parabirimi1) * tutar;
+                
+                // Tl den istenilene
+                parabirimi(comboBox2.Text);
+                string parabirimi2 = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='" + para + "']/ForexBuying").InnerXml;
+                parabirimi2 = parabirimi2.Replace(".", ",");
+
+                parabirimi2 = parabirimi2.Substring(0, parabirimi2.Length - 1);
+                para2 = para1 / float.Parse(parabirimi2);
+
+                txtsonuc.Text = para2.ToString();
+
+            }            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+        }
+        
+    }
+}
